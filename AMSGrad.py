7b6eeb69b0700957b4b4ebd86fa45a5abe18246c
Junho Kim <takis0112@gmail.com>
2018-01-23 23:04:11 +0900

    code

--- AMSGrad.py
+++ AMSGrad.py
@@ -5,6 +5,7 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import variable_scope
 from tensorflow.python.training import optimizer
 from tensorflow.python.ops import state_ops
+from tensorflow.python.ops import resource_variable_ops
 
 
 class AMSGrad(optimizer.Optimizer):
@@ -80,8 +81,81 @@ class AMSGrad(optimizer.Optimizer):
         var_update = state_ops.assign_sub(var, lr * m_t / (v_sqrt + epsilon_t), use_locking=self._use_locking)
         return control_flow_ops.group(*[var_update, m_t, v_t, vhat_t])
 
+    def _resource_apply_dense(self, grad, var):
+        var = var.handle
+        beta1_power = math_ops.cast(self._beta1_power, var.dtype.base_dtype)
+        beta2_power = math_ops.cast(self._beta2_power, var.dtype.base_dtype)
+        lr_t = math_ops.cast(self._lr_t, var.dtype.base_dtype)
+        beta1_t = math_ops.cast(self._beta1_t, var.dtype.base_dtype)
+        beta2_t = math_ops.cast(self._beta2_t, var.dtype.base_dtype)
+        epsilon_t = math_ops.cast(self._epsilon_t, var.dtype.base_dtype)
+
+        lr = (lr_t * math_ops.sqrt(1 - beta2_power) / (1 - beta1_power))
+
+        # m_t = beta1 * m + (1 - beta1) * g_t
+        m = self.get_slot(var, "m").handle
+        m_scaled_g_values = grad * (1 - beta1_t)
+        m_t = state_ops.assign(m, beta1_t * m + m_scaled_g_values, use_locking=self._use_locking)
+
+        # v_t = beta2 * v + (1 - beta2) * (g_t * g_t)
+        v = self.get_slot(var, "v").handle
+        v_scaled_g_values = (grad * grad) * (1 - beta2_t)
+        v_t = state_ops.assign(v, beta2_t * v + v_scaled_g_values, use_locking=self._use_locking)
+
+        # amsgrad
+        vhat = self.get_slot(var, "vhat").handle
+        vhat_t = state_ops.assign(vhat, math_ops.maximum(v_t, vhat))
+        v_sqrt = math_ops.sqrt(vhat_t)
+
+        var_update = state_ops.assign_sub(var, lr * m_t / (v_sqrt + epsilon_t), use_locking=self._use_locking)
+        return control_flow_ops.group(*[var_update, m_t, v_t, vhat_t])
+
+    def _apply_sparse_shared(self, grad, var, indices, scatter_add):
+        beta1_power = math_ops.cast(self._beta1_power, var.dtype.base_dtype)
+        beta2_power = math_ops.cast(self._beta2_power, var.dtype.base_dtype)
+        lr_t = math_ops.cast(self._lr_t, var.dtype.base_dtype)
+        beta1_t = math_ops.cast(self._beta1_t, var.dtype.base_dtype)
+        beta2_t = math_ops.cast(self._beta2_t, var.dtype.base_dtype)
+        epsilon_t = math_ops.cast(self._epsilon_t, var.dtype.base_dtype)
+
+        lr = (lr_t * math_ops.sqrt(1 - beta2_power) / (1 - beta1_power))
+
+        # m_t = beta1 * m + (1 - beta1) * g_t
+        m = self.get_slot(var, "m")
+        m_scaled_g_values = grad * (1 - beta1_t)
+        m_t = state_ops.assign(m, m * beta1_t, use_locking=self._use_locking)
+        with ops.control_dependencies([m_t]):
+            m_t = scatter_add(m, indices, m_scaled_g_values)
+
+        # v_t = beta2 * v + (1 - beta2) * (g_t * g_t)
+        v = self.get_slot(var, "v")
+        v_scaled_g_values = (grad * grad) * (1 - beta2_t)
+        v_t = state_ops.assign(v, v * beta2_t, use_locking=self._use_locking)
+        with ops.control_dependencies([v_t]):
+            v_t = scatter_add(v, indices, v_scaled_g_values)
+
+        # amsgrad
+        vhat = self.get_slot(var, "vhat")
+        vhat_t = state_ops.assign(vhat, math_ops.maximum(v_t, vhat))
+        v_sqrt = math_ops.sqrt(vhat_t)
+        var_update = state_ops.assign_sub(var, lr * m_t / (v_sqrt + epsilon_t), use_locking=self._use_locking)
+        return control_flow_ops.group(*[var_update, m_t, v_t, vhat_t])
+
     def _apply_sparse(self, grad, var):
-        return self._apply_dense(grad, var)
+        return self._apply_sparse_shared(
+            grad.values, var, grad.indices,
+            lambda x, i, v: state_ops.scatter_add(  # pylint: disable=g-long-lambda
+                x, i, v, use_locking=self._use_locking))
+
+    def _resource_scatter_add(self, x, i, v):
+        with ops.control_dependencies(
+                [resource_variable_ops.resource_scatter_add(
+                    x.handle, i, v)]):
+            return x.value()
+
+    def _resource_apply_sparse(self, grad, var, indices):
+        return self._apply_sparse_shared(
+            grad, var, indices, self._resource_scatter_add)
 
     def _finish(self, update_ops, name_scope):
         # Update the power accumulators.
